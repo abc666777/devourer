eaf78ad1b28597ddc7a624e4c0bf1593a9fbc41d
abc666777 <pptvstudio@gmail.com>
2020-08-23 10:43:48 +0700

    add PlayerController.cs

---
+++ Devourer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject[] arrayOfMonsters;
+    public GameObject[] arrayOfItems;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        InvokeRepeating(Instantiate(arrayOfMonsters[Random.Range(0, 1)]), 2.0f, 0.3f);
+        InvokeRepeating(Instantiate(arrayOfItems[Random.Range(0, 1)]), 5f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnMonsters(GameObject ob)
+    {
+
+    }
+}
---
+++ Devourer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 100;
+
+    private const float speedPenalty = 0.2f;
+
+    public bool isAffected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) isAffected = !isAffected;
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float movingPenalty = isAffected ? speedPenalty : 1;
+        transform.Translate(Vector2.up * (speed * Time.deltaTime * y * movingPenalty));
+        transform.Translate(Vector2.right * (speed * Time.deltaTime * x * movingPenalty));
+    }
+}
